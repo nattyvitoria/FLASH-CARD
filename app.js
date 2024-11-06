8bef5964263a190bcba89e5fd59c87c3c86a108b
nattyvitoria <160952292+nattyvitoria@users.noreply.github.com>
2024-11-06 17:25:26 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-function criaCartao(categoria, pergunta, resposta){
-    console.log(categoria, pergunta, resposta);
+function  criaCartao(categoria, pergunta, resposta) {
+let container = document.gertElementById('container');
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
 }
+cartao.innerHTML =
+<div class="cartao__conteudo"> </div>
+<h3>programaçao</h3>
+<div class="cartao__conteudo__pergunta">
+   <p> o que é javasricprit?</p>
+    <div class="cartao__conteudo__resposta">
+       <p> o javasricprit e uma linguagem de programaçao</p> 
